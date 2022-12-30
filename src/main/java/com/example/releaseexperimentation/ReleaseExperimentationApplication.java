995fd0c919ce01e1c890c0b172e5a13fae86ab8a
nabil belakbir <nabilblk@gmail.com>
2022-12-30 16:43:37 +0100

    feat: allow provided config object to extend other configs
    BREAKING CHANGE: `extends` key in config file is now used for extending other config files

---
+++ src/main/java/com/example/releaseexperimentation/ReleaseExperimentationApplication.java
@@ -0,0 +1,13 @@
+package com.example.releaseexperimentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseExperimentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseExperimentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/releaseexperimentation/ReleaseExperimentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.releaseexperimentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseExperimentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
